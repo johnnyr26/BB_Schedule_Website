e045e5b6e0ff68143868a326d01a6c5b2943c62d
Johnny <ramirezjohnny@icloud.com>
2024-06-21 09:10:58 -0700

    Fix bug not displaying correct error message

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,8 @@ router.get('/', function(req, res, next) {
         key: 'AIzaSyCpijldE4DDH_fTsapnbzFUtYYCDwutZG4'
     }, (error, response) => {
         if (error || !response.data.values) {
-            return console.log('The API returned an error: ' + err);
+            console.log('The API returned an error: ' + error);
+            return;
         }
         const schedules = response.data.values.flat();
         const courses = [];
